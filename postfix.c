edd690e9f30a3c41d8f66584e4604035b219f9ca
Andrew Graham <andrew.t.graham@live.com>
2015-08-11 13:15:50 -0700

    Repair Format

--- postfix.c
+++ postfix.c
@@ -9,11 +9,12 @@ int main (void)
 	
 	/*	Expected Output: 	1 2 3
 	 *	Actual Output:		2 1 3
-	 *	Explanation:			The gcc compiler has freedom to evaluate in any order it wants - unless 
-	 *									  forced with (). This leads to an interesting result in which the unary 
-	 *										increment operator (postfix), ++, is first evaluated at the middle term, 
-	 *										then the left most term (1 + 1: from the first i++), and finally i is 
-	 *										evaluated at the last position (2 + 1: from the left-most i++).  
+	 *	Explanation:			
+	 *		The gcc compiler has freedom to evaluate in any order it wants - unless 
+	 *		forced with (). This leads to an interesting result in which the unary 
+	 *		increment operator (postfix), ++, is first evaluated at the middle term, 
+	 *		then the left most term (1 + 1: from the first i++), and finally i is 
+	 *		evaluated at the last position (2 + 1: from the left-most i++).  
 	 */
 	printf("%d %d %d\n", i++, i++, i);
 
